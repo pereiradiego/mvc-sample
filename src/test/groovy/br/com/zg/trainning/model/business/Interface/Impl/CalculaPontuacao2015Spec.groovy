9d9608073bb70dc4a754e0d53856d16c94d9b0e3
pereiradiego <diego.zeroglosa@gmail.com>
2016-04-11 10:32:27 -0300

    Ticket #3 - Criação de mecanismo de desempate - modificação do metodo que aplica os critérios de desempate
    Correções contidas no ticket #3

---
+++ src/test/groovy/br/com/zg/trainning/model/business/Interface/Impl/CalculaPontuacao2015Spec.groovy
@@ -0,0 +1,32 @@
+package br.com.zg.trainning.model.business.Interface.Impl
+
+import spock.lang.Specification
+import br.com.zg.trainning.model.entity.Time
+import br.com.zg.trainning.model.business.Interface.CalculaPontuacao
+
+/**
+ * Created by diegoalves on 11/04/16.
+ */
+class CalculaPontuacao2015Spec extends Specification {
+	def "CalculaPontuacaoTotal"() {
+		setup:
+		Time time = new Time()
+		CalculaPontuacao2015 calculaPontuacao = new CalculaPontuacao2015()
+		int pontuacao
+
+		when:
+		time.idTime = 1
+		time.nomeTime = 'Ponte Preta'
+		time.quantVitoria = 10
+		time.quantDerrota = 11
+		time.quantEmpate = 4
+		time.quantGolPro = 14
+		time.quantGolContra = 23
+
+		pontuacao = calculaPontuacao.CalculaPontuacaoTotal(time)
+
+
+		then:
+		pontuacao == 34
+	}
+}
